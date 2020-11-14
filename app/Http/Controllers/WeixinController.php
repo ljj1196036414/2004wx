1d12f07d8f768499e49765e16045a03bfb4ac348
lijunjing <1196036414@qq.com>
2020-11-14 09:35:14 +0800

    赋权

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -166,7 +166,9 @@ class WeixinController extends Controller
             $urls="https://api.weixin.qq.com/sns/userinfo?access_token=".$access_token."&openid=".$openid."&lang=zh_CN";
             $userss = file_get_contents($urls);
             file_put_contents('logs.log', $userss."\n\n",FILE_APPEND);//记录日志
+
             $users = json_decode($userss,true);
+            dd($users);
             echo "6";
             return $users;
         }
